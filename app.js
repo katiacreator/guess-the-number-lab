b8c651d00b9a85e7f9171463037f53e7fb611d52
Katia Gibson <kcygibson@gmail.com>
2021-06-25 14:04:39 -0400

    added initial game code, no functionality yet

---
+++ app.js
@@ -0,0 +1,10 @@
+const game = {
+    title: 'Guess the Number!',
+    biggestNum: 100,
+    smallestNum: 1,
+    secretNum: null,
+    play: function() {
+      this.secretNum = Math.floor(Math.random() * 
+        (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+    }
+  }
