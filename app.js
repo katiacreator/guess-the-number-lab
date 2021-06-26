e1c285167a3e8a7de482f3c37b770ea655fc343d
Katia Gibson <kcygibson@gmail.com>
2021-06-26 15:52:54 -0400

    game works in the while loop, game does not end after number is picked

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const game = {
   title: "Guess the Number!",
-  biggestNum: 100,
+  biggestNum: 5,
   smallestNum: 1,
   secretNum: null,
   play: function () {
@@ -12,24 +12,39 @@ const game = {
 //Assign the secret number before prompt
 game.play();
 console.log(game.secretNum);
-// prompt to enter their guess
 
-let guess
-let prevGuess// equal to prevGuesses[i]
-let prevGuesses = []//push all prevGuesses to the array but only prevGuesses[array.length] = guess
+// prompt to enter their guess
+/* ### variables to create
+- guess, guessArray, guess is updated to result of prompt; prevGuess is guessArray[guessArray.length - 1] , numGuesses = guessArray.length, guessString, and the following 
+### alert template literals
+- tooLowAlert = `${prevGuess} is too low. you've guessed ${guessString}.`
+- tooHighAlert = `${prevGuess} is too high. you've guessed ${guessString}.`
+- winAlert = `Yes, ${prevGuess} was the secret number. You took ${numGuesses} guesses.`
+### functions/methods to create
+-while loop, prompt, if/else statment comparing guess to secret number */
+
+let guess; //guess is updated to result of prompt
+let prevGuess; // guessArray[guessArray.length - 1]
+const guessArray = []; //push all prevGuesses to the array but only prevGuesses[array.length] = guess
+let guessString; //guessArray.join(",")
+
 while (guess !== game.secretNum) {
-  guess = prompt("Enter a number between 0 and 100");
+  guess = prompt(
+    "Enter a number between 0 and 5 to guess the secret number!"
+  );
   if (guess < game.secretNum) {
     console.log(`${guess}`);
-    prevGuess = guess
-    alert(`${guess} is too low!`);
+    // prevGuess = guess
+    alert(`${guess} is too low. you've guessed ${guessString}.`);
   } else if (guess > game.secretNum) {
     console.log(`${guess}`);
-    prevGuess = guess
-    alert(`${guess} is too high`);
+    //prevGuess = guess
+    alert(`${guess} is too high. you've guessed ${guessString}.`);
   } else {
     console.log(`${guess}`);
-    prevGuess = guess
-    console.log(`${guess} was the secret number!`);
+    //prevGuess = guess
+    alert(
+      `Yes! ${guess} was the secret number! You took some guesses.`
+    );
   }
 }
