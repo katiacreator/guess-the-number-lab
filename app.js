ec92f14bdec0f93a9ad512ff1e513e485a72e92f
Katia Gibson <kcygibson@gmail.com>
2021-06-27 20:01:03 -0400

    cleaned up readme by transferring details to pseudocode readme file, removed additional js script tags from index.htmlcleaned up prompt loop so that game ends with break, and fixed guessString error. all bugs fixed

--- app.js
+++ app.js
@@ -26,30 +26,33 @@ const game = {
   let guess; //guess is updated to result of prompt
   let prevGuess; 
   const guessArray = []; //push all prevGuesses to the array but only prevGuesses[array.length] = guess
-  let guessString = guessArray.join(", ")
+  let guessString;
   
   while (guess !== game.secretNum) {
-    guess = prompt(
-      "Enter a number between 0 and 100 to guess the secret number!"
-    );
-    if (guess < game.secretNum) {
-      prevGuess = guess;
-      guessArray.push(prevGuess);
-      console.log(`${prevGuess}`);
-      console.log(guessArray);
-      alert(`${guess} is too low. you've guessed ${guessString}.`);
-    } else if (guess > game.secretNum) {
-      prevGuess = guess;
-      guessArray.push(prevGuess);
-      console.log(`${prevGuess}`);
-      console.log(guessArray);
+      guess = prompt(
+          "Enter a number between 0 and 100 to guess the secret number!"
+        );
+        if (guess < game.secretNum) {
+            prevGuess = guess;
+            guessArray.push(prevGuess);
+            console.log(`${prevGuess}`);
+            console.log(guessArray);
+        guessString = guessArray.join(" , ")
+            alert(`${guess} is too low. you've guessed ${guessString}.`);
+          } else if (guess > game.secretNum) {
+              prevGuess = guess;
+              guessArray.push(prevGuess);
+              console.log(`${prevGuess}`);
+              console.log(guessArray);
+          guessString = guessArray.join(" , ")
       alert(`${guess} is too high. you've guessed ${guessString}.`);
     } else {
       prevGuess = guess;
       guessArray.push(prevGuess);
       console.log(`${prevGuess}`); 
       console.log(guessArray);
-      alert(`Yes! ${guess} was the secret number! You took ${guessArray.length} guesses. Press ok to end this game and start a new game.`);
+      alert(`Yes! ${guess} was the secret number! You took ${guessArray.length} guesses. Press ok to end this game.`);
+      break;
     }
   }
   
