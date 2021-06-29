765acd5681285ff9239441fbe2d34c81773a0c88
Katia Gibson <kcygibson@gmail.com>
2021-06-28 21:57:31 -0400

    confirmed submit button works at grabbing the input

--- dynamicApp.js
+++ dynamicApp.js
@@ -11,15 +11,18 @@ const game = {
   },
 };
 
-// console.log(game.instructions);
+game.instructions = "Pick a number between 0-100 to reveal the secret number!";
+console.log(game.instructions);
 game.guess; //guess is updated to result of html form input
 //console.log(game.guess)
-game.prevGuess;
+// game.prevGuess;
 game.guessArray = []; //push all prevGuesses to the guessArray
 //console.log(game.guessArray)
 game.guessString = game.guessArray.join(", ");
 //console.log(game.guessString)
-console.log(game);
+// console.log(game);
+game.lastGuess = game.guessArray[game.guessArray.length - 1];
+game.isWinner = false
 
 /*------------------------ Cached Element References ------------------------*/
 
@@ -32,12 +35,11 @@ console.log("game.p: ", game.p);
 game.section = document.querySelectorAll(".game-section");
 //This is the form div where numInput lives
 game.form = document.querySelector("form");
-console.log("form: ", game.form);
+//console.log("form: ", game.form);
 
 // to select the number guess
 game.numInput = document.getElementById("section-number-input");
-console.log(`Bug, value is: ${game.numInput.value}`); //BUG FIXED!!!!
-
+console.log(`Bug, value is: ${game.numInput.value}`); 
 // to SELECT secret number Div to append secret num
 game.secretNumBox = document.getElementById("section-secret-number-box");
 game.secretNumBox.textContent;
@@ -49,7 +51,7 @@ game.guessLog = document.getElementById("section-guess-log");
 //to display buttons
 game.startEnd = document.getElementById("section-start-end");
 
-/* BUTTON SELECTORS */
+/* BUTTON SELECTORS FOR EVENT LISTENERS */
 //when number is submitted in input and button is clicked number will show in the prevlog box after it is add to guessArray
 game.submitBtn = document.getElementById("submitBtn");
 //console.log(game.submitBtn)
@@ -58,106 +60,130 @@ game.startBtn = document.getElementById("startBtn");
 game.resetBtn = document.getElementById("resetBtn");
 //console.log(game.resetBtn)
 
-/*----------------------------- Event Listeners -----------------------------*/
-
-
-  
-
-
-//To reset game
-game.resetBtn.addEventListener("click", function () {
-  console.log("resetButton is clicked");
-  // game.toggle(game.p);
-  // game.guessLogText = "";
-  // game.guessLog.textContent = game.guessLogText;
-  // game.section.forEach((section) => {
-  //   game.toggle(section);
-  // });
-  // game.startEnd.remove(game.resetBtn);
-});
-
-// form.addEventListener("reset", init);
-
-//To submit guess to check Input function
-game.form.addEventListener("submit", function (evt) {
-  evt.preventDefault(); //prevents page from reloading on input
-  console.log(game.numInput.value, `submitbutton clicked`)
-  // if (!isWinner) {
-  //   checkGuess(parseInt(game.numInput.value));
-  // }
-});
-
 /*-------------------------------- Functions --------------------------------*/
 
 /** FUNCTIONS to be called **/
 //const toggle;//toggle class from who to hide-use query selector
 game.toggle = function (element) {
   element.classList.toggle("hide");
-}; 
+};
 
+//starts the entire app
 game.init = function () {
   console.log("startButton is clicked");
-  game.guessLogText = "";
-  game.toggle(game.p);
+  game.p.innerText = game.instructions;
+  game.guessLog.textContent =
+    "Psst! Hey you! Look here to see if you guessed correctly or if you guessed too low/high.";
   game.secretNumBox.textContent = "?";
-  game.guessLogText = " ";
   game.section.forEach((section) => {
     game.toggle(section);
   });
   game.startEnd.remove(game.startBtn);
   game.resetBtn.setAttribute("hidden", true);
   game.guessArray = [];
-  isWinner = false;
+  
   //Call game play function to generate secret number here
   game.play();
   //This is the secret number
-  console.log(game.secretNum);
+  console.log(`${game.secretNum} is the secret number. delete this after testing.`);
   // render();
 };
 
-//To start the game
-game.startBtn.addEventListener("click", game.init)
+//Checks to see if the number entered is valid
+game.errorCheck = function () {
+  game.guess = game.numInputVal;
+  console.log(game.guess)
+  if (isNaN(game.guess) || game.guess < 1 || game.guess > 100) {
+    game.renderError("Whoops! Please enter a number from 1 to 100.");
+    return; //we dont want that error guess pushed to the array!
+  } else if (game.guess === game.secretNum) {
+    game.isWinner = true;
+  }
+  game.guessArray.push(game.guess);
+  game.render();
+}
 
-
-
-/* game.gameOver = function () {
-  game.prevGuess = game.guess;
-  game.guessArray.push(game.prevGuess);
-  //reveal secretnumber
-  game.secretNumBox.textContent = game.secretNum;
-  //change guess log text
-  game.guessLogText = `Yes, ${game.guess} was the secret number! You took ${game.guessArray.length} guess(es). Press reset button to start a new game`;
-  game.guessLog.textContent = game.guessLogText;
-  // reveal reset button-BUG
-};  */
-
-/* game.checkInput = function () {
-  while (game.guess !== game.secretNum) {
-    game.guess = parseInt(game.numInput.value);
-    if (game.guess < game.secretNum) {
-      game.prevGuess = game.guess;
-      game.guessArray.push(game.prevGuess);
-      // console.log(`${game.prevGuess}`);
-      // console.log(game.guessArray);
-      game.guessLog.textContent = `${game.prevGuess} was too low. You've guessed ${game.guessString} so far.`;
-    } else if (game.guess > game.secretNum) {
-      game.prevGuess = game.guess;
-      game.guessArray.push(game.prevGuess);
-      // console.log(`${game.prevGuess}`);
-      // console.log(game.guessArray);
-      game.guessLog.textContent = `${game.prevGuess} was too high. You've guessed ${game.guessString} so far.`;
-    } else {
-      game.gameOver();
-    }
+// compares number from input to secret number and logs that guess
+game.compareInput = function () {
+  game.lastGuess = parseInt(game.numInput.value);
+  if (game.lastGuess < game.secretNum) {
+    game.lastGuess = game.guess;
+    game.guessArray.push(game.guess);
+    // console.log(`${game.prevGuess}`);
+    // console.log(game.guessArray);
+    game.guessLog.textContent = `Oof! ${game.lastGuess} was too low. Don't forget, you've guessed ${game.guessString} so far.`;
+  } else if (game.lastGuess < game.secretNum) {
+    game.lastGuess = game.guess;
+    game.guessArray.push(game.guess);
+    // console.log(`${game.prevGuess}`);
+    // console.log(game.guessArray);
+    game.guessLog.textContent = `Oof! ${game.lastGuess} was too high. Don't forget, you've guessed ${game.guessString} so far.`;
   }
 };
 
-game.init = function () {
-  game.toggle(game.p);
-  game.secretNumBox.textContent = "?";
-  game.guessLogText = " ";
-  game.section.forEach((section) => {
-    game.toggle(section);
-  });
-  game.startEnd.remove(game.startBtn);
-}; */
+//needs to be defined before game.render/renders textxContengt of guess log div with celebratory win statement esp if they guessed in 1 try
+game.renderWin = function (div) {
+  div = game.secretNumBox;
+  div.textContent = game.secretNum;
+  game.guessArray.push(game.guess);
+  game.guessLog.textContent = `Yes! ${game.lastGuess} was the secret number! You took ${game.guessArray.length} guess(es). Press reset button to see if you can do that again!`;
+  if (game.guessArray.length === 1) {
+    game.guessLog.textContent = `You're a wizard. You just guessed the secret number: ${game.lastGuess} in one guess! Press reset to see if you can do that again!`;
+  } else {
+    game.guessLog.textContent = `Congratulations! You found the secret number ${game.secretNum} in ${game.guessArray.length} guesses!`;
+  }
+};
+
+
+
+//Renders the text content for the guesses log div
+game.render = function () {
+  if (game.guessArray.length === 1) {
+    game.guessArray.push(game.guess);
+    game.resetBtn.removeAttribute("hidden");
+  }
+
+  if (game.isWinner) {
+    game.renderWin(div);
+  } else if (
+    game.lastGuess > game.secretNum ||
+    game.lastGuess < game.secretNum
+  ) {
+    game.compareInput();
+  }
+};
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+//To start the game
+game.startBtn.addEventListener("click", game.init);
+//posts error message in header p tag if number is not valid
+game.renderError = function() {
+  game.p.textContent = "error";
+  game.toggle(p)
+}
+
+// game.form.addEventListener("reset", game.init);
+//To reset game
+game.resetBtn.addEventListener("reset", game.init);
+  
+
+//To submit guess to check Input function
+game.form.addEventListener("submit", function (e) {
+  game.numInputVal = parseInt(game.numInput.value)
+  e.preventDefault(); //prevents page from reloading on input
+  console.log(game.numInputVal, `submitbutton clicked`);
+  if (!game.isWinner) {
+    game.errorCheck(game.numInputVal);
+  }
+});
+
+
+
+
+
+
+
+
+
+
